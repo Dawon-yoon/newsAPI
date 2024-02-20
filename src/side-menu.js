7ce99f48dc41546f084f86bab0017a9bd8f571eb
Dawon-yoon <dawon3887@gmail.com>
2024-02-20 22:29:15 +0900

    #3

---
+++ src/side-menu.js
@@ -0,0 +1,15 @@
+const sideMenu=document.getElementById("side-menu-id");
+
+const openNav=()=>{
+    sideMenu.style.width="250px";
+};
+
+const closeNav=()=>{
+sideMenu.style.width="0";
+}
+
+const openSearchBox=()=>{
+    let inputArea=document.getElementById("input-area");
+    if(inputArea.style.display==="inline"){
+        inputArea.style.display="none";    }else{inputArea.style.display="inline";}
+}
